7df3e5a6d6d58ff5217732af6e3a83c0185e3fff
Dikesh01 <kabir33559@gmail.com>
2023-07-01 17:39:14 +0530

    Shopping_Cart

---
+++ profilePage/script.js
@@ -0,0 +1,8 @@
+
+const fname = document.getElementById('firstName');
+const lName = document.getElementById('lastName');
+
+let obj = JSON.parse(sessionStorage.getItem('userLoggedIn'));
+
+fname.value = obj.firstName;
+lName.value = obj.lastName;
